af4a3c0332b139e56812eb61f8daefc559152e0f
Damilola Alimi <alimidammy02@gmail.com>
2026-05-03 22:41:35 +0100

    changes

--- ios/cmreactnative/AppDelegate.mm
+++ ios/cmreactnative/AppDelegate.mm
@@ -13,18 +13,13 @@
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
-- (NSURL *)bundleURL
+- (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
 #if DEBUG
   return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
 #else
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
-}
-
-- (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
-{
-  return [self bundleURL];
 }
 
 @end
